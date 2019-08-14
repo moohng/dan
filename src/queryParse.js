6320bae8313bc940ac283b481d78c3751305b86f
Kevin <moohng@126.com>
2019-08-14 22:24:30 +0800

    test

--- src/queryParse.js
+++ src/queryParse.js
@@ -1,10 +1,15 @@
 export default function queryParse(qs = '') {
-  return qs.match(/[^&]+/g).reduce((query, matched) => {
-    const t = matched.match(/[^=?]+/g)
+  if (typeof qs !== 'string') return {}
+  const startIndex = qs.lastIndexOf('?')
+  if (startIndex < 0) return {}
+  return qs.slice(startIndex + 1).match(/[^&]+/g).reduce((query, matched) => {
+    const t = matched.match(/[^=]+/g)
+    const value = t[1]
+    if (typeof value === 'undefined') return query
     const hasKey = query[t[0]]
     if (hasKey !== undefined) {
-      return { ...query, [t[0]]: [].concat(hasKey, t[1]) }
+      return { ...query, [t[0]]: [].concat(hasKey, value) }
     }
-    return { ...query, [t[0]]: t[1] }
+    return { ...query, [t[0]]: value }
   }, {})
 }
